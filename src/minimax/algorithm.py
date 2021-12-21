c52ab159ba4f46d134b0074fea0fb36da741b3b1
iadams749 <iwa332@email.unc.edu>
2021-12-21 12:37:04 -0500

    Simplified minimaxabp method

--- src/minimax/algorithm.py
+++ src/minimax/algorithm.py
@@ -49,63 +49,7 @@ def minimax(depth, max_player, game, move):
 
 
 def minimaxabp(depth, max_player, game, move, alpha, beta):
-    if depth == 0 or game.gameOver:
-        return game.calcScore()
-
-    if (max_player):
-        moves = game.getValidMoves()
-        bestMove = [-1, -1]
-        bestScore = -9999
-
-        for m in moves:
-            ga = deepcopy(game)
-            ga.doTurn(m[0], m[1])
-            if (ga.turn == -1):
-                score = minimaxabp(depth - 1, False, ga, move, alpha, beta)
-            else:
-                score = minimaxabp(depth - 1, True, ga, move, alpha, beta)
-            if score > bestScore:
-                bestMove[0] = m[0]
-                bestMove[1] = m[1]
-                bestScore = score
-
-            alpha = max(alpha, bestScore)
-
-            if beta <= alpha:
-                break
-
-        move[0] = bestMove[0]
-        move[1] = bestMove[1]
-
-        return bestScore
-
-    else:
-        moves = game.getValidMoves()
-        bestMove = [-1, -1]
-        bestScore = 9999
-
-        for m in moves:
-            ga = deepcopy(game)
-            ga.doTurn(m[0], m[1])
-            if (ga.turn == -1):
-                score = minimaxabp(depth - 1, False, ga, move, alpha, beta)
-            else:
-                score = minimaxabp(depth - 1, True, ga, move, alpha, beta)
-
-            if score < bestScore:
-                bestMove[0] = m[0]
-                bestMove[1] = m[1]
-                bestScore = score
-
-            beta = min(beta, bestScore)
-
-            if beta <= alpha:
-                break
-
-        move[0] = bestMove[0]
-        move[1] = bestMove[1]
-
-        return bestScore
+    return minimaxabpweighted(depth, max_player, game, move, alpha, beta, [1, 1, 1, 1, 1, 1])
 
 
 def minimaxabpweighted(depth, max_player, game, move, alpha, beta, values):
